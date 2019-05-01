0b501c7d0c79371b3b7e3249c7e0057979b0211a
Michael <michael@mapletonhill.net>
2019-05-01 13:18:02 -0600

    fix up this reference in gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,5 @@
 module.exports = (grunt) => {
-    grunt.registerTask('default', 'Example TeamCity Progress Messaging', () => {
+    grunt.registerTask('default', 'Example TeamCity Progress Messaging', function() {
         const done = this.async();
         grunt.log.writeln("##teamcity[progressMessage 'An example progress message. Waiting 5 seconds']");
         setTimeout(() => {
